b7d5a7169c79080840e0f90fd509770663004f77
yogen45 <yogendra.bhattarai@outcodesoftware.com>
2025-01-07 19:28:23 +0545

    Added Candidate model class

---
+++ WebApplication1/Model/Candidate.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Model
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+        [EmailAddress]
+        public required string Email { get; set; }
+        public string? CallTimeInterval { get; set; }
+        public string? LinkedInURL { get; set; }
+        public string? GitHubURL { get; set; }
+        public required string FreeComment { get; set; }
+
+    }
+}
