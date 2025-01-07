37a8be0c11d1aff69fe8ea5407cf2b16b5666e10
yogen45 <yogendra.bhattarai@outcodesoftware.com>
2025-01-07 20:53:27 +0545

    Implemented Repository to communicate with DbContext

---
+++ WebApplication1/Repository/CandidateRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Database.Context;
+using WebApplication1.Model;
+using WebApplication1.Repository.Interface;
+
+namespace WebApplication1.Repository
+{
+    public class CandidateRepository : ICandidateRepository
+    {
+        private readonly CandidateManagementAPIDbContext _context;
+        public CandidateRepository(CandidateManagementAPIDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Candidate> AddCandidate(Candidate candidate)
+        {
+            await _context.Candidates.AddAsync(candidate);
+            await _context.SaveChangesAsync();
+            return candidate;
+
+        }
+
+        public async Task<Candidate?> GetCandidateByEmail(string candidateEmail)
+        {
+            var candidate = await _context.Candidates.Where(x => x.Email == candidateEmail).FirstOrDefaultAsync();
+            return candidate;
+        }
+
+        public async Task<Candidate?> GetCandidateById(int candidateId)
+        {
+            var candidate = await _context.Candidates.FindAsync(candidateId);
+            return candidate;
+        }
+
+        public async Task<Candidate> UpdateCandidate(Candidate candidate)
+        {
+            _context.Candidates.Update(candidate);
+            await _context.SaveChangesAsync();
+            return candidate;
+        }
+    }
+}
---
+++ WebApplication1/Repository/Interface/ICandidateRepository.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication1.Model;
+
+namespace WebApplication1.Repository.Interface
+{
+    public interface ICandidateRepository
+    {
+        Task<Candidate> AddCandidate(Candidate candidate);
+        Task<Candidate> UpdateCandidate(Candidate candidate);
+        Task<Candidate?> GetCandidateById(int candidateId);
+        Task<Candidate?> GetCandidateByEmail(string email);
+    }
+}
